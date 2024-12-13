59fbff07f99502cba95e2769073851d3503f89c1
NishantRajhans <nishantrajhans35@gmail.com>
2024-12-13 12:05:39 +0530

    add .env file

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { Telegraf } from "telegraf";
 import { message } from 'telegraf/filters';
-const bot = new Telegraf('7993069629:AAEDQ8NZlnKtn4GGPXYNcWMbM-hFP1Pg3ps');
+import dotenv from "dotenv"
+dotenv.config()
+const bot = new Telegraf(process.env.TELEGRAM_BOT);
 bot.on(message("text"), ctx => ctx.reply("Hello"));
 bot.launch({
   webhook: {
-    domain:  "https://kind-friends-feel.loca.lt",
+    domain: process.env.WEBHOOK_DOMAIN,
     port: 4000,
   },
 });
