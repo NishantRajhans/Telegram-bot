3dbd9988f35b21d48f807aedcd54a8b4bdae43c9
NishantRajhans <nishantrajhans35@gmail.com>
2024-12-13 12:50:15 +0530

    add make changes in domain

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import { Telegraf } from "telegraf";
 import { message } from 'telegraf/filters';
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 const bot = new Telegraf(process.env.TELEGRAM_BOT);
-bot.on(message("text"), ctx => ctx.reply("Hello"));
+bot.on(message("text"), ctx => {
+    ctx.reply("Hello welcome");
+});
 bot.launch({
   webhook: {
-    domain: process.env.WEBHOOK_DOMAIN,
-    port: 4000,
+    domain: "https://telegram-bot-kappa-three.vercel.app",
+    port:5003,
   },
 });
